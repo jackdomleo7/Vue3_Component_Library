0ec6a3ced0d4c542e8026aa09585cb997894c568
Jack Domleo <jackdomleo7@gmail.com>
2021-12-17 15:36:54 +0000

    Lint

--- src/components/Sample/Sample.stories.ts
+++ src/components/Sample/Sample.stories.ts
@@ -1,24 +1,24 @@
-import { Story } from '@storybook/vue3'
-import { JSample } from '@/components'
+import { Story } from "@storybook/vue3";
+import { JSample } from "@/components";
 
 export default {
-  title: 'Components/Sample',
+  title: "Components/Sample",
   component: JSample,
   parameters: {
     docs: {
       description: {
-        component: "`import { JSample } from '@jackdomleo7/vue3-library'`"
-      }
-    }
-  }
-}
+        component: "`import { JSample } from '@jackdomleo7/vue3-library'`",
+      },
+    },
+  },
+};
 
 const Template = (args: Record<string, unknown>) => ({
   components: { JSample },
-  setup () {
-    return { args }
+  setup() {
+    return { args };
   },
-  template: '<JSample v-bind="args" />'
-})
+  template: '<JSample v-bind="args" />',
+});
 
-export const Default: Story = Template.bind({})
+export const Default: Story = Template.bind({});

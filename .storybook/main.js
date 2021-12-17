6f127d9872a97f15f4decadb71f4c3634a3591e6
Jack Domleo <jackdomleo7@gmail.com>
2021-12-17 14:47:12 +0000

    Allow Storybook to understand @ aliasing routes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,17 @@
+const path = require("path")
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/vue3"
+  framework: "@storybook/vue3",
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+    return config
+  }
 }

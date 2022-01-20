5c8874e03a90b29cae7868a177206f0a49e73a9a
Jack Domleo <jackdomleo7@gmail.com>
2022-01-20 14:26:40 +0000

    Update Storybook packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
+    "plugin:storybook/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2020,

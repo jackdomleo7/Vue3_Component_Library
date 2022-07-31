b5be2959501e4564795c3b6ebf0f91e1657804bd
Jack Domleo <jackdomleo7@gmail.com>
2022-07-31 15:15:29 +0100

    More thorough tests

--- src/components/HelloWorld/HelloWorld.spec.ts
+++ src/components/HelloWorld/HelloWorld.spec.ts
@@ -34,4 +34,32 @@ describe.concurrent('HelloWorld', () => {
       expect(wrapper).toMatchSnapshot()
     })
   })
+
+  describe('props', () => {
+    it('displays the message "Hello there!"', () => {
+      const wrapper = mount(JHelloWorld, {
+        props: {
+          msg: 'Hello there!'
+        }
+      })
+
+      expect(wrapper.find('h1').text()).toBe('Hello there!')
+    })
+  })
+
+  describe('interactivity', () => {
+    it('increments the counter when clicked', async () => {
+      const wrapper = mount(JHelloWorld, {
+        props: {
+          msg: 'Hello there!'
+        }
+      })
+
+      expect(wrapper.find('button').text()).toBe('count is 0')
+
+      await wrapper.find('button').trigger('click')
+
+      expect(wrapper.find('button').text()).toBe('count is 1')
+    })
+  })
 })

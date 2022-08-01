efea0348b2f83937d7e348553f6eb0b5dd375062
Jack Domleo <jackdomleo7@gmail.com>
2022-08-01 22:50:28 +0100

    Add @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    extensions: ['.ts', '.js', '.vue'],
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  },
+  plugins: [vue(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),

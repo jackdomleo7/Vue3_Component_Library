3a5e0ec65d5af115bcd7da64578488a99c1181df
Jack Domleo <jackdomleo7@gmail.com>
2023-08-28 19:07:50 +0100

    Add theming option to Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,9 @@
-import type { Preview } from '@storybook/vue3'
+import { type Preview } from '@storybook/vue3'
 import '../src/styles/main.scss'
 
 const preview: Preview = {
   parameters: {
+    backgrounds: { disable: true },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
@@ -15,7 +16,40 @@ const preview: Preview = {
         order: ['Docs', 'Setup', 'Components']
       }
     }
-  }
+  },
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Choose a built-in theme',
+      defaultValue: 'light',
+      toolbar: {
+        dynamicTitle: true,
+        items: [
+          { value: 'light', title: 'Default', icon: 'sun' },
+          { value: 'dark', title: 'Dark', icon: 'moon' }
+        ]
+      }
+    }
+  },
+  decorators: [
+    (story, context) => {
+      return {
+        data() {
+          return {
+            theme: context.globals.theme
+          }
+        },
+        created() {
+          document.body.setAttribute(
+            'class',
+            this.theme !== 'light' ? `theme--${this.theme}` : ''
+          )
+        },
+        components: { story },
+        template: '<story />'
+      }
+    }
+  ]
 }
 
 export default preview

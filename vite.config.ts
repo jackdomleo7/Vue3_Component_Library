dc1256ea163977bb99e18512f577db91d331208e
Jack Domleo <jackdomleo7@gmail.com>
2023-12-24 18:32:28 +0000

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import pkg from './package.json';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    extensions: ['.ts', '.js', '.vue'],
+    extensions: ['.mts', '.ts', '.mjs', '.js', '.vue'],
     alias: [
       {
         find: '@',

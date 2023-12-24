b05fff46b4e89ff5fa826b5ef4f41a27b65a6d1b
Jack Domleo <jackdomleo7@gmail.com>
2023-12-24 18:22:40 +0000

    Delete JHelloWorld

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default as JButton } from './components/Button/Button.vue'
-export { default as JHelloWorld } from './components/HelloWorld/HelloWorld.vue'
 export { default as JIcon } from './components/Icon/Icon.vue'

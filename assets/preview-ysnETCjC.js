11308c9134c2ab135b6f1d291d559fc8768f76df
jackdomleo7 <43371432+jackdomleo7@users.noreply.github.com>
2024-10-08 20:57:48 +0000

    Deploying to gh-pages from @ jackdomleo7/Vue3_Component_Library@a55c4b8caec18cb43bc41f2a0c16345577cea45e 🚀

--- assets/preview-ysnETCjC.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-mztS001h.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-4kloecQq.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-4kloecQq.js","./iframe-mztS001h.js","./index-inUycwjX.js","./_commonjsHelpers-5-cIlDoe.js","./index-ogXoivrg.js","./index-OpicemMp.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
---
+++ assets/typography-aHPTfGjq.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-zOX4ABP6.js";import{M as o,d as m}from"./index-xMzYnogK.js";import{u as i}from"./index-AVjP0WR2.js";import"./iframe-SVWwBtlM.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-5-cIlDoe.js";import"./index-ogXoivrg.js";import"./index-OpicemMp.js";import"./index-PPLHz8o0.js";const s={type:{primary:"Arial, sans-serif"},weight:{regular:"400",bold:"700"},size:{"text-xs":12,"text-sm":14,"text-md":16,"text-lg":18,"text-xl":20,"text-xxl":24}},x="Lorem ipsum dolor sit amet, consectetur adipiscing elit.";function n(r){const e=Object.assign({h1:"h1",p:"p",strong:"strong"},i(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(o,{title:"Docs/Typography"}),`
+`,`
+`,`
+`,t.jsx(e.h1,{id:"typography",children:"Typography"}),`
+`,t.jsxs(e.p,{children:[t.jsx(e.strong,{children:"Font:"})," Arial"]}),`
+`,t.jsxs(e.p,{children:[t.jsx(e.strong,{children:"Weights:"})," 400(regular), 700(bold)"]}),`
+`,t.jsx(m,{fontSizes:[Number(s.size["text-xs"]),Number(s.size["text-sm"]),Number(s.size["text-md"]),Number(s.size["text-lg"]),Number(s.size["text-xl"]),Number(s.size["text-xxl"])],fontWeight:s.weight.bold,sampleText:x,fontFamily:s.type.primary})]})}function y(r={}){const{wrapper:e}=Object.assign({},i(),r.components);return e?t.jsx(e,Object.assign({},r,{children:t.jsx(n,r)})):n(r)}export{x as SampleText,y as default,s as typography};

1de51da165c53b802f13a0651953dd1746f81083
jackdomleo7 <43371432+jackdomleo7@users.noreply.github.com>
2025-01-18 13:34:05 +0000

    Deploying to gh-pages from @ jackdomleo7/Vue3_Component_Library@f127be56c4d0c4a9ed95e8472c6ae19abbd9d70f 🚀

--- components/Button/Button.vue.d.ts
+++ components/Button/Button.vue.d.ts
@@ -1,90 +1,46 @@
-import type { Components, Tokens } from '../../types';
-declare const _default: __VLS_WithTemplateSlots<import('./vue/dist/vue.esm-bundler.js').DefineComponent<__VLS_WithDefaults<__VLS_TypePropsToRuntimeProps<{
+import { Components, Tokens } from '../../types';
+type __VLS_Props = {
     /**
      * The element to use.
      * @default "<button>"
      */
-    tag?: Components.Button.Tag | undefined;
+    tag?: Components.Button.Tag;
     /**
      * The button type. _Ignored if not `<button>`._
      * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#type
      * @default "button"
      */
-    type?: Components.Button.Type | undefined;
+    type?: Components.Button.Type;
     /**
      * _Ignored if `<button>`._
      */
-    href?: string | undefined;
-    size?: Tokens.FontSizeValue | undefined;
-    round?: boolean | undefined;
-    status?: Components.Button.Status | undefined;
-    outline?: boolean | undefined;
-    icon?: Components.Button.Icon | undefined;
-    disabled?: boolean | undefined;
-}>, {
-    tag: string;
-    type: string;
-    href: undefined;
-    size: string;
-    status: undefined;
-    icon: undefined;
-}>, {}, unknown, {}, {}, import('./vue/dist/vue.esm-bundler.js').ComponentOptionsMixin, import('./vue/dist/vue.esm-bundler.js').ComponentOptionsMixin, {}, string, import('./vue/dist/vue.esm-bundler.js').VNodeProps & import('./vue/dist/vue.esm-bundler.js').AllowedComponentProps & import('./vue/dist/vue.esm-bundler.js').ComponentCustomProps, Readonly<import('./vue/dist/vue.esm-bundler.js').ExtractPropTypes<__VLS_WithDefaults<__VLS_TypePropsToRuntimeProps<{
-    /**
-     * The element to use.
-     * @default "<button>"
-     */
-    tag?: Components.Button.Tag | undefined;
-    /**
-     * The button type. _Ignored if not `<button>`._
-     * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#type
-     * @default "button"
-     */
-    type?: Components.Button.Type | undefined;
-    /**
-     * _Ignored if `<button>`._
-     */
-    href?: string | undefined;
-    size?: Tokens.FontSizeValue | undefined;
-    round?: boolean | undefined;
-    status?: Components.Button.Status | undefined;
-    outline?: boolean | undefined;
-    icon?: Components.Button.Icon | undefined;
-    disabled?: boolean | undefined;
-}>, {
-    tag: string;
-    type: string;
-    href: undefined;
-    size: string;
-    status: undefined;
-    icon: undefined;
-}>>>, {
+    href?: string;
+    size?: Tokens.FontSizeValue;
+    round?: boolean;
+    status?: Components.Button.Status;
+    outline?: boolean;
+    icon?: Components.Button.Icon;
+    disabled?: boolean;
+};
+declare function __VLS_template(): {
+    attrs: Partial<{}>;
+    slots: {
+        default?(_: {}): any;
+    };
+    refs: {};
+    rootEl: any;
+};
+type __VLS_TemplateResult = ReturnType<typeof __VLS_template>;
+declare const __VLS_component: import('vue').DefineComponent<__VLS_Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {
     icon: Components.Button.Icon;
     type: Components.Button.Type;
     size: Tokens.FontSizeValue;
     tag: Components.Button.Tag;
     href: string;
     status: Components.Button.Status;
-}, {}>, {
-    default?(_: {}): any;
-}>;
+}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, any>;
+declare const _default: __VLS_WithTemplateSlots<typeof __VLS_component, __VLS_TemplateResult["slots"]>;
 export default _default;
-type __VLS_NonUndefinedable<T> = T extends undefined ? never : T;
-type __VLS_TypePropsToRuntimeProps<T> = {
-    [K in keyof T]-?: {} extends Pick<T, K> ? {
-        type: import('./vue/dist/vue.esm-bundler.js').PropType<__VLS_NonUndefinedable<T[K]>>;
-    } : {
-        type: import('./vue/dist/vue.esm-bundler.js').PropType<T[K]>;
-        required: true;
-    };
-};
-type __VLS_WithDefaults<P, D> = {
-    [K in keyof Pick<P, keyof P>]: K extends keyof D ? __VLS_Prettify<P[K] & {
-        default: D[K];
-    }> : P[K];
-};
-type __VLS_Prettify<T> = {
-    [K in keyof T]: T[K];
-} & {};
 type __VLS_WithTemplateSlots<T, S> = T & {
     new (): {
         $slots: S;
--- components/Icon/Icon.vue.d.ts
+++ components/Icon/Icon.vue.d.ts
@@ -1,16 +1,6 @@
-import { type PropType } from './vue/dist/vue.esm-bundler.js';
-import type { Components } from '../../types';
-declare const _default: import('./vue/dist/vue.esm-bundler.js').DefineComponent<{
-    icon: {
-        type: PropType<Components.Icon.Icon>;
-        required: true;
-        validator: (icon: Components.Icon.Icon) => boolean;
-    };
-}, {}, unknown, {}, {}, import('./vue/dist/vue.esm-bundler.js').ComponentOptionsMixin, import('./vue/dist/vue.esm-bundler.js').ComponentOptionsMixin, {}, string, import('./vue/dist/vue.esm-bundler.js').VNodeProps & import('./vue/dist/vue.esm-bundler.js').AllowedComponentProps & import('./vue/dist/vue.esm-bundler.js').ComponentCustomProps, Readonly<import('./vue/dist/vue.esm-bundler.js').ExtractPropTypes<{
-    icon: {
-        type: PropType<Components.Icon.Icon>;
-        required: true;
-        validator: (icon: Components.Icon.Icon) => boolean;
-    };
-}>>, {}, {}>;
+import { Components } from '../../types';
+type __VLS_Props = {
+    icon: Components.Icon.Icon;
+};
+declare const _default: import('vue').DefineComponent<__VLS_Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, any>;
 export default _default;

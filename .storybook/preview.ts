63ff188b51b77984fc2634833b31fd8c5276629d
Jack Domleo <jackdomleo7@gmail.com>
2025-04-05 21:50:00 +0100

    docs(Storybook): Use expanded Storybook controls

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,6 +6,7 @@ const preview: Preview = {
     backgrounds: { disable: true },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/
